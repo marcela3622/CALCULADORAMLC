0412fff8a06c48e2833a442322b5f3f14376103a
marcela3622 <marcelapadilla0705@gmail.com>
2023-09-27 18:18:58 -0600

    PRIMER AVANCE DE LA CALCULADORA MARCELA

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora PHP</title>
+    <style>
+        input[type="text"] {
+            width: 200px;
+        }
+    </style>
